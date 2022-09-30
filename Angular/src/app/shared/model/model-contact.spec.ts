1b5e9e61ea0c0195f02c315fdec8db807becc7da
Otemis2 <113303311+Otemis2@users.noreply.github.com>
2022-09-30 20:16:07 +0200

    Delete Angular/src directory

--- Angular/src/app/shared/model/model-contact.spec.ts
+++
@@ -1,7 +0,0 @@
-import { ModelContact } from './model-contact';
-
-describe('ModelContact', () => {
-  it('should create an instance', () => {
-    expect(new ModelContact()).toBeTruthy();
-  });
-});
